a5ce70d724eef34f54cab376032af604ead939f7
Sema-1054531 <1054531@hr.nl>
2023-03-05 18:02:19 +0100

    updated: adding classes

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import sqlite3
 import os
 
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), '/lib/databasewp3.db')
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'lib/databasewp3.db')
 
 LISTEN_ALL = "0.0.0.0"
 FLASK_IP = LISTEN_ALL
@@ -53,11 +53,17 @@ def close_checkin():
 
 @app.route('/plan_bijeenkomst', methods=['GET', 'POST'])
 def plan_bijeenkomst():
+    db = get_db()
+
     if request.method == 'POST':
         title = request.form['title']
         datemeeting = request.form['datemeeting']
         start_time = request.form['start_time']
         end_time = request.form['end_time']
+        if 'class' in request.form:
+            classid = request.form['class']
+        else:
+            classid = None
 
         # validate the input
         if not title:
@@ -70,17 +76,17 @@ def plan_bijeenkomst():
             return 'Vul een eind tijd in'
         if start_time >= end_time:
             return 'De start tijd moet voor de eind tijd liggen'
-        if datemeeting < datetime.now().strftime('%d-%m-%Y'):
+        datemeeting = datetime.strptime(request.form['datemeeting'], '%Y-%m-%d')
+        if datemeeting.date() < datetime.now().date():
             return 'De datum ligt in het verleden!'
 
-        db = get_db()
-        db.execute("INSERT INTO meeting (title, datemeeting, start_time, end_time) VALUES (?, ?, ?, ?)",
-                   (title, datemeeting, start_time, end_time))
+        db.execute("INSERT INTO meeting (title, datemeeting, start_time, end_time, classid) VALUES (?, ?, ?, ?, ?)",
+                   (title, datemeeting.strftime('%Y-%m-%d'), start_time, end_time, classid))
         db.commit()
 
         return 'bijeenkomst aangemaakt'
-    else:
-        return render_template("bijeenkomst_plannen.html")
+    classes = db.execute('SELECT * from class').fetchall()
+    return render_template("bijeenkomst_plannen.html", classes=classes)
 
 
 if __name__ == "__main__":
